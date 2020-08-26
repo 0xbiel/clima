575972a4482024f131abda6d1e3330bac9d35add
Biel <bielp07@gmail.com>
2020-08-26 09:16:17 -0300

    requests working

--- clima.py
+++ clima.py
@@ -2,4 +2,11 @@
 
 import requests
 
-api = "http://api.openweathermap.org/data/2.5/weather?q=[city]&appid=[apikey]"
+#api = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format()
+
+api = "http://api.openweathermap.org/data/2.5/weather?q=Sao+Paulo&appid=cd70123bfc02f39978477a22ad0efcfa".format()
+
+
+req = requests.get(api)
+data = req.json()
+print(data)
